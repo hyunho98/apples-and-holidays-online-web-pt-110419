33398158b4d46e2ca1aebe828cdc623c0c2a59e4
Alex Lee <hyunho98@gmail.com>
2019-11-12 09:48:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,6 +71,7 @@ def all_supplies_in_holidays(holiday_hash)
     holidays.each{|day, supplies|
       puts "  " + "#{day}: ".capitalize + supplies.join(", ")}
     }
+    puts output
 end
 
 def all_holidays_with_bbq(holiday_hash)
